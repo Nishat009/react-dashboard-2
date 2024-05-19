4b21587d020e00a915df0f9cc09ea11d6a0a9db0
Prantik0616 <prantik.techvill@gmail.com>
2024-05-19 14:17:00 +0600

    market tab work

---
+++ src/Components/tab.js
@@ -0,0 +1,113 @@
+import React from 'react'
+import { Tab } from '@headlessui/react'
+
+const categories = [
+    {
+      name: 'Recent',
+      posts: [
+        {
+          id: 1,
+          title: 'Does drinking coffee make you smarter?',
+          date: '5h ago',
+          commentCount: 5,
+          shareCount: 2,
+        },
+        {
+          id: 2,
+          title: "So you've bought coffee... now what?",
+          date: '2h ago',
+          commentCount: 3,
+          shareCount: 2,
+        },
+      ],
+    },
+    {
+      name: 'Popular',
+      posts: [
+        {
+          id: 1,
+          title: 'Is tech making coffee better or worse?',
+          date: 'Jan 7',
+          commentCount: 29,
+          shareCount: 16,
+        },
+        {
+          id: 2,
+          title: 'The most innovative things happening in coffee',
+          date: 'Mar 19',
+          commentCount: 24,
+          shareCount: 12,
+        },
+      ],
+    },
+    {
+      name: 'Trending',
+      posts: [
+        {
+          id: 1,
+          title: 'Ask Me Anything: 10 answers to your questions about coffee',
+          date: '2d ago',
+          commentCount: 9,
+          shareCount: 5,
+        },
+        {
+          id: 2,
+          title: "The worst advice we've ever heard about coffee",
+          date: '4d ago',
+          commentCount: 1,
+          shareCount: 2,
+        },
+      ],
+    },
+  ]
+function tab() {
+
+  return (
+    <div className="flex w-full justify-center pt-4">
+      <div className="w-full max-w-md">
+        <Tab.Group>
+          <Tab.List className="flex gap-4">
+            {categories.map(({ name }) => (
+              <Tab
+                key={name}
+                className={({ selected }) =>
+                  `rounded-full py-1 px-3 text-sm font-semibold text-black focus:outline-none ${
+                    selected ? 'bg-black/10' : 'hover:bg-white/5'
+                  }`
+                }
+              >
+                {name}
+              </Tab>
+            ))}
+          </Tab.List>
+          <Tab.Panels className="mt-3">
+            {categories.map(({ name, posts }) => (
+              <Tab.Panel key={name} className="rounded-xl bg-white/5 ">
+                <ul>
+                  {posts.map((post) => (
+                    <li key={post.id} className=" rounded-md pb-1 pt-1 text-sm transition hover:bg-white/5">
+                      <a href="javaScript:void()" className="font-semibold text-black">
+                        <span className=" inset-0" />
+                        {post.title}
+                      </a>
+                      <ul className="flex gap-2 text-white/50" aria-hidden="true">
+                        <li>{post.date}</li>
+                        <li aria-hidden="true">&middot;</li>
+                        <li>{post.commentCount} comments</li>
+                        <li aria-hidden="true">&middot;</li>
+                        <li>{post.shareCount} shares</li>
+                      </ul>
+                    </li>
+                  ))}
+                </ul>
+              </Tab.Panel>
+            ))}
+          </Tab.Panels>
+        </Tab.Group>
+      </div>
+    </div>
+
+  )
+}
+
+export default tab
--- src/page/dashboard.js
+++ src/page/dashboard.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Navbar from '../layouts/navbar'
 import Sidebar from '../layouts/sidebar';
-// import ChartBar from '../Components/dashboardDetails';
+import Tab from '../Components/tab';
 import {cardData} from '../data/currencyData';
 import CurrencyCard from '../Components/CurrencyCard'
 import ChartBar from '../Components/ChartBars';
@@ -13,7 +13,7 @@ function dashboard() {
             <div className="h-screen sidebar-nav md:fixed z-[100] md:z-50  left-0 w-[270px] text-color-14 flex flex-col top-[80px]">
                 <Sidebar/>
             </div>
-            <div className="w-[84.5%] pt-10 9xl:px-[245px] 7xl:px-[135px] 5xl:px-[67px] px-5 pb-28 overflow-auto main-content flex flex-col flex-1 left-[280px] dark:bg-[#292929] border-l dark:border-[#474746] border-color-DF h-screen absolute">
+            <div className="w-[84.5%] pt-10 pb-28 main-content flex flex-col flex-1 left-[280px] dark:bg-[#292929] border-l dark:border-[#474746] pl-5 border-color-DF h-screen absolute">
                 
               <div>
                   <div className='grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-2 min-[1620px]:grid-cols-4 gap-[25px]'>
@@ -29,9 +29,9 @@ function dashboard() {
                     <div className="flex-auto bg-[#f8f6f6] dark:bg-[#373741] rounded-[15px]"> 
                       <ChartBar/>
                     </div>
-                    <div className="bg-[#dfdfdf] dark:bg-[#393944] rounded-[15px] py-5 pl-[39px] pr-[20px] xl:w-[478px] relative mt-[25px] xl:mt-0">
-                      <p className='absolute left-[39px] top-[37px] text-[#2c2c2c] text-[20px] font-semibold leading-[28px] dark:text-white'>Markets</p>
-                        {/* <TabsRender /> */}
+                    <div className="bg-[#f8f6f6] dark:bg-[#373741] rounded-[15px] py-5 pl-[39px] pr-[20px] xl:w-[478px] relative mt-[25px] xl:mt-0">
+                      <p className=' text-[#2c2c2c] text-[20px] font-semibold leading-[28px] dark:text-white'>Markets</p>
+                        <Tab/>
                     </div>
                   </div>
           
